ba9da801b8fd86e3739825a9cb8021d919f986ee
emi <kairosart2007@gmail.com>
2018-11-05 14:36:41 +0100

    Testing with scikit-learn

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, session
 import random
+from sklearn import datasets, svm
+
 app = Flask(__name__)
 
 # Add these extra two lines
@@ -28,6 +30,17 @@ def factors_display(n):
 	number=n, 
 	factors=factors(n) 
 	)
+
+# Load Dataset from scikit-learn.
+digits = datasets.load_digits()
+
+@app.route('/predict')
+def hello():
+    clf = svm.SVC(gamma=0.001, C=100.)
+    clf.fit(digits.data[:-1], digits.target[:-1])
+    prediction = clf.predict(digits.data[-1:])
+
+return jsonify({'prediction': repr(prediction)})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
