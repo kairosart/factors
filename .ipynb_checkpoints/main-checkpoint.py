09f894bd6264596702072dee3d19e972bc1924ea
emi <kairosart2007@gmail.com>
2018-11-05 14:46:06 +0100

    Testing with scikit-learn

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -34,7 +34,7 @@ def factors_display(n):
 # Load Dataset from scikit-learn.
 digits = datasets.load_digits()
 
-@app.route('/predict')
+@app.route('/predict', methods=['POST', 'GET'])
 def hello():
     clf = svm.SVC(gamma=0.001, C=100.)
     clf.fit(digits.data[:-1], digits.target[:-1])
