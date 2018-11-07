8254e484b6e99be66bd878488be8484fa7a4846d
emi <kairosart2007@gmail.com>
2018-11-07 20:44:48 +0100

    Creating stock values chart

--- main.py
+++ main.py
@@ -5,6 +5,11 @@ import numpy as np
 import random
 import datetime as dt
 from sklearn import datasets, svm
+import plotly
+import plotly.plotly as py
+import plotly.graph_objs as go
+import json
+from markupsafe import Markup
 
 # To fetch data
 from pandas_datareader import data as pdr   
@@ -12,6 +17,10 @@ import fix_yahoo_finance as yf
 yf.pdr_override()  
 
 from util import create_df_benchmark, get_data, fetchOnlineData
+from strategyLearner import strategyLearner
+from marketsim import compute_portvals_single_symbol, market_simulator
+from indicators import get_momentum, get_sma, get_sma_indicator, compute_bollinger_value, get_RSI, plot_cum_return,  plot_momentum, plot_sma_indicator, plot_rsi_indicator, plot_momentum_sma_indicator, plot_stock_prices
+
 
 app = Flask(__name__)
 
@@ -44,9 +53,12 @@ def showvalues():
     symbol = request.form['symbol']
     portf_value = fetchOnlineData(start_d, yesterday, symbol)
     
+    # Create Stock prices chart
+    my_plot_div = plot_stock_prices(portf_value.index, portf_value['Adj Close'], symbol)
+    
     return render_template(
     # name of template
-	"showvalues.html",
+	"base.html",
         
     # now we pass in our variables into the template
     start_val = request.form['start_val'],
@@ -57,8 +69,11 @@ def showvalues():
     start_date = start_d, 
     end_date = yesterday,
     tables=[portf_value.to_html(classes=symbol)],
-    titles = ['na', 'Price']    
+    titles = ['na', 'Stock Prices '],
+    div_placeholder=Markup(my_plot_div)  
     )  
+
+
 
 # Initial form to get values
 @app.route('/form', methods = ['GET', 'POST'])
