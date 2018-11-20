9dca495506d825926a3e5522459c49ee09b6503e
emi <kairosart2007@gmail.com>
2018-11-20 22:32:48 +0100

    Changing stockpriceschart  button color and text

--- static/js/my_jquery.js
+++ static/js/my_jquery.js
@@ -4,11 +4,12 @@ $(document).ready(function(e){
     $("#submitButton").on('click',function(){
 
         $(this).val("Wait a second...");
-        $(this).css('color','red');
+        $(this).css('color','white');
+        $(this).css('background-color', 'red');
     });
 	$("#submitButton1").on('click',function(){
-
-        $(this).val("Calculating...");
-        $(this).css('color','red');
+		$(this).html('Calculating...');
+        $(this).css('color','white');
+        $(this).css('background-color', 'red');
     });    
 });
