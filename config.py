1faecc6f244fbee10481d8fa02e083fe6ff78e00
emi <kairosart2007@gmail.com>
2018-11-22 14:12:44 +0100

    Adding background tasks.

--- config.py
+++ config.py
@@ -1,3 +1,5 @@
 WTF_CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 SESSION_TYPE = 'filesystem'
+CELERY_BROKER_URL = 'redis://localhost:6379/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
